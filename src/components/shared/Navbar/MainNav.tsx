923bfc4a0077fe9bcd483282e77be78c8aeddb7d
SriArunM <msriarunm@gmail.com>
2024-12-02 21:10:33 +0530

    Initial commit

---
+++ src/components/shared/Navbar/MainNav.tsx
@@ -0,0 +1,103 @@
+import { Link, NavLink } from 'react-router-dom';
+import { Button } from '../../../components/ui/button';
+import bikeLogo from '../../../assets/icons/Bike logo.png';
+import { logout, useCurrentUser } from '@/redux/features/auth/authSlice';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+
+export const MainNav = () => {
+  const user = useAppSelector(useCurrentUser);
+  const dispatch = useAppDispatch();
+
+  const handleSignOut = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <div className='mr-4 hidden gap-2 md:flex justify-between items-center w-full my-1'>
+      <div>
+        <Link to={'/'} className='text-blue-400'>
+          <img src={bikeLogo} alt='Bike Rental Logo' />
+          <h1 className='text-center text-xl font-bold'>Bike Rental</h1>
+        </Link>
+      </div>
+      <nav id='navbar' className='flex gap-2'>
+        <NavLink to={'/'}>
+          {({ isActive }) => (
+            <Button
+              variant='link'
+              className={`text-xl ${
+                isActive ? 'text-blue-600' : 'hover:text-blue-500'
+              }`}
+            >
+              Home
+            </Button>
+          )}
+        </NavLink>
+
+        <NavLink to={'/about-us'}>
+          {({ isActive }) => (
+            <Button
+              variant='link'
+              className={`text-xl ${
+                isActive ? 'text-blue-600' : 'hover:text-blue-500'
+              }`}
+            >
+              About Us
+            </Button>
+          )}
+        </NavLink>
+
+        <NavLink to={'/dashboard/profile'}>
+          {({ isActive }) => (
+            <Button
+              variant='link'
+              className={`text-xl ${
+                isActive ? 'text-blue-600' : 'hover:text-blue-500'
+              }`}
+            >
+              Dashboard
+            </Button>
+          )}
+        </NavLink>
+
+        <NavLink to={'/all-bikes'}>
+          {({ isActive }) => (
+            <Button
+              variant='link'
+              className={`text-xl ${
+                isActive ? 'text-blue-600' : 'hover:text-blue-500'
+              }`}
+            >
+              All Bikes
+            </Button>
+          )}
+        </NavLink>
+
+        {!user && (
+          <NavLink to={'/signIn'}>
+            {({ isActive }) => (
+              <Button
+                variant='link'
+                className={`text-xl ${
+                  isActive ? 'text-blue-600' : 'hover:text-orange-500'
+                }`}
+              >
+                Sign In
+              </Button>
+            )}
+          </NavLink>
+        )}
+
+        {user && (
+          <Button
+            variant='secondary'
+            className='text-xl text-red-600'
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </Button>
+        )}
+      </nav>
+    </div>
+  );
+};
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,24 @@
+import { fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store';
+
+// https://bike-rental-reservation-system-backend-coral.vercel.app/api
+
+const baseQuery = fetchBaseQuery({
+  baseUrl:
+    'https://bike-rental-reservation-system-backend-coral.vercel.app/api',
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+export const baseApi = createApi({
+  reducerPath: 'baseApi',
+  baseQuery: baseQuery,
+  tagTypes: ['user', 'bike', 'rental'],
+  endpoints: () => ({}),
+});
